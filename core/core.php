b9474a5bd5b3f6fc5f3dc63491dec5c099b36eb2
TallyThemes <admin@tallythemes.com>
2015-03-09 14:02:24 +0600

    Adding Home page options

--- core/core.php
+++ core/core.php
@@ -275,6 +275,7 @@ class tally_loader{
 		tally_file_dri('options/_o_fonts.php');
 		tally_file_dri('options/_o_social-icons.php');
 		tally_file_dri('options/_o_footer.php');
+		tally_file_dri('options/_o_home.php');
 		tally_file_dri('options/_o_blog-page.php');
 		tally_file_dri('options/_o_contact-page.php');
 		tally_file_dri('options/_o_404-page.php');
---
+++ demo/hconfig.php
@@ -0,0 +1,82 @@
+<?php
+$tally_hconfig = array( 
+	array(
+		'id' => 'slideshow',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'blogGrid',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'contact',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'infoBox',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'people',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'quoteSlider',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'slideshow',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'textBlock',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'textBlock2',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'textBlock3',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'textBlock4',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'textBlock5',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+	array(
+		'id' => 'videoText',
+		'biz' => false,
+		'div_class' => '',
+		'div_id' => '',
+	),
+);
+return $tally_hconfig;
